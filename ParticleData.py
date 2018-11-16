033e54dc952c4393d64c8bb495df8cef27aa205a
sabareesh169 <sabareesh169@gmail.com>
2018-11-16 17:11:59 -0500

    Update ParticleData.py

--- ParticleData.py
+++ ParticleData.py
@@ -1,27 +1,38 @@
 """
 
-This contains .
+This class takes in the particle data and scales the data for better functioning of the DNN's.
+The postional data is normalised w.r.t the initial position of the particles. 
+The time data is 'normalized' differently to avoid (potentially) division with zero.
 
 """
 
 class ParticleData:
 
-    def __init__(self, initial, final, t_initial, t_final, vel_layers, radius):
+    def __init__(self, initial_pos, final_pos, t_initial, t_final, vel_layers, radius):
         self.sess = tf.Session()
-        self.initial = initial.astype(np.float32)
-        self.final = final.astype(np.float32)
+        self.initial_pos = initial_pos.astype(np.float32)
+        self.final_pos = final_pos.astype(np.float32)
         self.t_initial = t_initial.astype(np.float32)
         self.t_final = t_final.astype(np.float32)
 
-        self.vel_weights, self.vel_biases = initialize_NN(vel_layers)
+        self.mean_pos = np.mean(initial_pos)
+        self.sigma_pos = np.std(initial_pos)
 
-        self.initial = train_data[:,[0,1]].astype(np.float32)
-        self.initial_ch, self.mu_train_pos, self.sigma_train_pos=rescale(initial)
-        self.final = train_data[:,[2,3]].astype(np.float32)
-        self.final_ch=rescale_test(final, self.mu_train_pos, self.sigma_train_pos)
-        self.t_scale = np.max(t_final)
-        self.t_initial_ch =t_initial/self.t_scale
-        self.t_final_ch = t_final/self.t_scale
+        self.max_time = np.max(t_final)
+        self.t_initial_norm = self.rescale_time_data(t_initial)
+        self.t_final_norm = self.rescale_time_data(t_final)
+
+        self.initial_pos_norm = self.rescale_pos_data(initial_pos)
+        self.final_pos_norm = self.rescale_pos_data(final_pos)
+        
+    def rescale_pos_data(self, array):
+        rescaled = (array - self.mean_pos)/ self.sigma_pos)
+        return rescaled
+    
+    def rescale_time_data(self, array):
+        rescaled =  array/ self.max_time
+        return rescaled
+
 
 
 
