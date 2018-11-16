096a888e7448344978355a3c95620980a6a6ab74
sabareesh169 <sabareesh169@gmail.com>
2018-11-16 15:38:20 -0500

    Update sampling.py

--- sampling.py
+++ sampling.py
@@ -3,27 +3,24 @@ def sampling_points(initial, final, pred, list_, sigma):
     for i in range(initial.shape[0]):
         points_index = np.array(list_[i])
         points = final[points_index]
-        p = np.exp(-np.sum((pred[i,:]-points)**2, axis=1)/(2*sigma_**2))/(2*sigma_)
+        p = np.exp(-np.sum((pred[i,:]-points)**2, axis=1)/(2*sigma**2))/(2*sigma)
         cum_sum_q=np.exp(np.log(p)-sc.misc.logsumexp(np.log(p))).cumsum()
         u=np.random.rand()
         idx=np.arange(len(cum_sum_q))
         index[i]=points_index[idx[u<cum_sum_q][0]]
     return index
 
-def sampling_sigma(optimizer_sigma, pred, final_index):
+def optimize_theta(self, true_vel_ch):
     for i in range(50):
-        optimzer_sigma.run(feed_dict={likelihood: np.sum((pred-final[index])**2)})
+        self.sess.run(self.optimizer_phy)
+        self.sess.run(self.optimizer_vel, feed_dict={self.vel_sample: true_vel_ch})
 
-def optimize_theta(optimzer_vel, optimizer_phy, true_vel_ch):
+def sampling_theta(self, optimizer_vel, initial, final_index, t_initial, t_final):
+    true_vel_ch = (final_index-initial)/(t_final-t_initial)
+    optimize_theta(self, true_vel_ch)
+
+def sampling_sigma(self, optimizer_sigma, pred, final_index):
     for i in range(50):
-        optimzer_vel.run(feed_dict={vel_sample: true_vel_ch})
-        optimzer_phy.run()
-        
-def sampling_theta(optimzer_vel, optimzer_phy, initial, final_index, t_initial, t_final, c_l, c_t):
-    sample_x_data_ch = final_index[:, 0][:, None].astype(np.float32)/c_l
-    sample_y_data_ch = final_index[:, 1][:, None].astype(np.float32)/c_l
-    
-    true_vel_x_ch = (sample_x_data_ch - initial_x_data_ch)/(t_final_data_ch - t_initial_data_ch)
-    true_vel_y_ch = (sample_y_data_ch - initial_y_data_ch)/(t_final_data_ch - t_initial_data_ch)
-    true_vel_ch = np.concatenate([true_vel_x_ch, true_vel_y_ch], axis=1)
-    optimize_theta(optimzer_vel, optimizer_phy, true_vel_ch)
+        self.sess.run(optimizer_sigma, feed_dict={self.likelihood: np.sum((pred-final_index)**2)})
+            
+
