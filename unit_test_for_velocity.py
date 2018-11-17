0f289d4050f2930d2aa51af0bcdf68b72745a3c2
sabareesh169 <sabareesh169@gmail.com>
2018-11-17 13:52:18 -0500

    Update unit_test_for_velocity.py

--- unit_test_for_velocity.py
+++ unit_test_for_velocity.py
@@ -6,7 +6,7 @@ t_final = 0.01
 radius = 4.
 rho = 1000.
 mu = 0.008
-vel_layers = [3, 100, 100, 2]
+vel_layers = [3, 100, 100, 3]
 
 test_data = ParticleData(initial_pos, final_pos, t_initial, t_final, radius)
 test_velocity_model = VelocityModel(test_data, vel_layers, rho, mu)
@@ -16,10 +16,9 @@ x_plot = np.linspace(0, 40, 100)
 y_plot = np.linspace(0, 40, 100)
 X,Y = np.meshgrid(x_plot, y_plot)
 
-with tf.Session() as sess:
-  test_training.predict_velocity(n_iter=500)
-  test_vel_pred = test_training.vel_predict(t_initial, t_final, X.reshape(-1,1), Y.reshape(-1,1))
-  test_abs_vel_pred = np.linalg.norm(test_vel_pred, axis=1)
+test_training.train_velocity(n_iter=500)
+test_vel_pred = test_training.vel_predict(t_initial, X.reshape(-1,1), Y.reshape(-1,1))
+test_abs_vel_pred = np.linalg.norm(test_vel_pred, axis=1)
 
 fig, ax = plt.subplots()
 
