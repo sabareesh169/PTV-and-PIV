6e96ed3211db1516c55e97a09c29be75a277e598
sabareesh169 <sabareesh169@gmail.com>
2018-11-17 00:04:25 -0500

    Delete scaling.py

--- scaling.py
+++
@@ -1,7 +0,0 @@
-def rescale(array):
-    rescaled = (array - np.mean(array))/ np.std(array)
-    return rescaled, np.mean(array), np.std(array)
-    
-def rescale_test(array, mean, sigma):
-    return (array - mean)/ sigma
-    
