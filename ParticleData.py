b12816847c6cbd85017bd7dd902863ccf69ede2f
sabareesh169 <sabareesh169@gmail.com>
2018-11-17 12:00:26 -0500

    Update ParticleData.py

--- ParticleData.py
+++ ParticleData.py
@@ -1,5 +1,4 @@
 """
-
 This class takes in the particle data and scales the data for better functioning of the DNN's.
 The postional data is normalised w.r.t the initial position of the particles. 
 The time data is 'normalized' differently to avoid (potentially) division with zero.
@@ -16,21 +15,21 @@ class ParticleData:
         self.sess = tf.Session()
         self.initial_pos = initial_pos.astype(np.float32)
         self.final_pos = final_pos.astype(np.float32)
-        self.t_initial = np.ones((initial_pos.shape[0],1))*t_initial
-        self.t_final = np.ones((final_pos.shape[0],1))*t_final
+        self.t_initial = np.float32(t_initial)
+        self.t_final = np.float32(t_final)
+        
+        self.mean_pos = np.mean(initial_pos,axis=0)
+        self.sigma_pos = np.std(initial_pos,axis=0)
 
-        self.mean_pos = np.mean(initial_pos)
-        self.sigma_pos = np.std(initial_pos)
-
-        self.max_time = t_final
-        self.t_initial_norm = self.rescale_time_data(t_initial)
-        self.t_final_norm = self.rescale_time_data(t_final)
+        self.max_time = self.t_final
+        self.t_initial_norm = self.rescale_time_data(t_initial, self.initial_pos)
+        self.t_final_norm = self.rescale_time_data(t_final, self.initial_pos)
 
         self.initial_pos_norm = self.rescale_pos_data(initial_pos)
         self.final_pos_norm = self.rescale_pos_data(final_pos)
 
-        tree=spatial.KDTree(initial)
-        self.cluster=tree.query_ball_point(initial, radius)
+        tree=spatial.KDTree(initial_pos)
+        self.cluster=tree.query_ball_point(initial_pos, radius)
         self.time_bound=[np.min(self.t_initial), np.max(self.t_final)]
         
     def rescale_pos_data(self, array):
@@ -39,13 +38,13 @@ class ParticleData:
         :param array: any spatial data to be normalized w.r.t the mean and variance of the initial position
         :returns: normalized data
         """
-        normalized_data = ((array - self.mean_pos)/ self.sigma_pos)
-        return rescaled
+        normalized_data = ((array - self.mean_pos)/ self.sigma_pos).astype(np.float32)
+        return normalized_data
     
     def rescale_time_data(self, time, position):
         """
         :param array: any temporal data to be scaled appropriately w.r.t to the final time
         :returns: normalized data
         """
-        normalized_data =  np.ones((position.shape[0],1))*time/ self.max_time
+        normalized_data =  (np.ones((position.shape[0],1))*time/ self.max_time).astype(np.float32)
         return normalized_data
