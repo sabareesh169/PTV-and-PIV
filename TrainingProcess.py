92aa5588170df1725c01c8e5d05dcea44139077d
sabareesh169 <sabareesh169@gmail.com>
2018-11-17 08:57:44 -0500

    Update TrainingProcess.py

--- TrainingProcess.py
+++ TrainingProcess.py
@@ -22,18 +22,25 @@ class TrainingProcess:
         :param size: Sampling the points, theta and sigma one after the other for n_iter number of times.
         :returns: optimized weights of the velocity DNN and the matched particles.
         """
-         for i in range(n_iter):
+        for i in range(n_iter):
             index=sampling_points(self.ParticleData.initial_pos, self.ParticleData.final_pos, self.sess.run(self.VelocityModel.pos_NN), self.ParticleData.cluster, self.sess.run(self.VelocityModel.sigma))
             final_index=self.ParticleData.rescale_pos_data(self.ParticleData.final_pos[index])
             sampling_theta(self, self.optimizer_vel, self.ParticleData.initial_pos, final_index, self.ParticleData.t_initial, self.ParticleData.t_final)
             sampling_sigma(self, self.optimizer_sigma, self.sess.run(self.VelocityModel.pos_NN), final_index)
         self.index=index  
 
-    def predict_velocity(self, n_iter):
+    def train_velocity(self, n_iter):
         """
         :param size: Sampling the theta and sigma one after the other for n_iter number of times.
         :returns: optimized weights of the velocity DNN.
         """
-         for i in range(n_iter):
+        for i in range(n_iter):
             sampling_theta(self, self.optimizer_vel, self.ParticleData.initial_pos, self.ParticleData.final_pos, self.ParticleData.t_initial, self.ParticleData.t_final)
             sampling_sigma(self, self.optimizer_sigma, self.sess.run(self.VelocityModel.pos_NN), self.ParticleData.final_pos)
+
+    def predict_velocity(self, t, x, y):
+        """
+        :param : the time and position of the point.
+        :returns: velocity at the particular position and time.
+        """
+        return self.sess.run(                    
