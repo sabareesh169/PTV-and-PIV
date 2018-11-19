4af134290b5152df1640c572dd7a369ea43a99fc
sabareesh169 <sabareesh169@gmail.com>
2018-11-19 11:29:50 -0500

    Update and rename sampling.py to ptv_piv_dnn/sampling.py

--- ptv_piv_dnn/sampling.py
+++ ptv_piv_dnn/sampling.py
@@ -1,4 +1,21 @@
+"""
+This is to run optimzation so as to sample points, theta and sigma alternatively (Gibb's sampling).
+
+Author:
+    Sabareesh Mamidipaka, Ilias Bilionis
+Date:
+    11/19/2018
+"""
+
+
 def sampling_points(initial, final, pred, list_, sigma):
+    """
+    Sampling the points by assigning a pmf according to the current prediction of the DNN.
+
+    :param array: the initial and final position, and the prediction and variance of the DNN
+    :returns: matching index for each point in the initial data.
+    """
+        
     index = np.arange(initial.shape[0])
     for i in range(initial.shape[0]):
         points_index = np.array(list_[i])
@@ -11,14 +28,31 @@ def sampling_points(initial, final, pred, list_, sigma):
     return index
 
 def optimize_theta(self, true_vel_ch):
+    """
+    Optimizing the DNN when the sample velocity is given.
+
+    :param array: the current 'true velocity' given the matching index.
+    """
+    
     for i in range(50):
         self.sess.run(self.optimizer_phy)
         self.sess.run(self.optimizer_vel, feed_dict={self.VelocityModel.vel_sample: true_vel_ch})
 
 def sampling_theta(self, optimizer_vel, initial, final_index, t_initial, t_final):
+    """
+    Optimizing the DNN when the sample velocity is given.
+
+    :param array: the initial and final position, and the prediction and variance of the DNN
+    :returns: matching index for each point in the initial data.
+    """
     true_vel_ch = (final_index-initial)/(t_final-t_initial)
     optimize_theta(self, true_vel_ch)
 
 def sampling_sigma(self, optimizer_sigma, pred, final_index):
+    """
+    Optimizing the variance given the likelihood.
+
+    :param array: the optimzer for variance, prediction and matched index given by the DNN
+    """
     for i in range(50):
         self.sess.run(optimizer_sigma, feed_dict={self.VelocityModel.likelihood : np.sum((pred-final_index)**2)})
