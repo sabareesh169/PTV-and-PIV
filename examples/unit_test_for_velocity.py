ff5abfd212df20e80d3eb2ba133056a4ace83795
sabareesh169 <sabareesh169@gmail.com>
2018-11-18 23:35:38 -0500

    Update unit_test_for_velocity.py

--- examples/unit_test_for_velocity.py
+++ examples/unit_test_for_velocity.py
@@ -1,38 +1,63 @@
 """
 Unit test demonstrating the use and application of the code.
-We generate random data set of 100 points for a uniform flow field in x-direction(200 m/s).
+We generate random data set of 100 points for a flow around a cylinder.
 We take in a training data of 100 points and sample theta and sigma 500 times to get a trained velocity model.
 We can then use this DNN to predict velocity at all points in space.
 """
 
-##  Generating a training data of uniformly spread particles in a 40*40 space
-X_c = lhs(2, samples=100, criterion='m')
-initial_pos = X_c*40.
-final_pos = initial_pos+np.array([2.,0])
-t_initial = 0.
-t_final = 0.01
-radius = 4.
+## Loading training and testing data
+train_data=pd.read_csv("foc_train_data.csv")
+test_data=pd.read_csv("foc_test_data.csv")
+train_data = train_data
+train_data = train_data.values
+test_data = test_data.values
+
+initial = train_data[:,[0,1]].astype(np.float32)
+final = train_data[:,[2,3]].astype(np.float32)
+t_initial = train_data[0,6].astype(np.float32)
+t_final = train_data[0,7]
+
+test_initial = test_data[:,[0,1]].astype(np.float32)
+test_final = test_data[:,[2,3]].astype(np.float32)
+
+radius = .05
 rho = 1000.
 mu = 0.008
 vel_layers = [3, 100, 100, 3]
 
 ##  Setting up ParticleData, VelocityModel and Training modules
-test_data = ParticleData(initial_pos, final_pos, t_initial, t_final, radius)
-test_velocity_model = VelocityModel(test_data, vel_layers, rho, mu)
-test_training = TrainingProcess(test_data, test_velocity_model)
-
-##  Test data set
-x_plot = np.linspace(0, 40, 100)
-y_plot = np.linspace(0, 40, 100)
-X,Y = np.meshgrid(x_plot, y_plot)
+data = ParticleData(initial, final, t_initial, t_final, radius)
+test_velocity_model = VelocityModel(data, vel_layers, rho, mu)
+test_training = TrainingProcess(data, test_velocity_model)
 
 ## Training for 500 iterations
 test_training.train_velocity(n_iter=500)
-test_vel_pred = test_training.vel_predict(t_initial, X.reshape(-1,1), Y.reshape(-1,1))
-test_abs_vel_pred = np.linalg.norm(test_vel_pred, axis=1)
+pred = test_training.pos_predict(t_initial, t_final, initial[:,0][:,None], initial[:,1][:,None])
+test_pred = test_training.pos_predict(t_initial, t_final, test_initial[:,0][:,None], test_initial[:,1][:,None])
 
-fig, ax = plt.subplots()
+## Final loss values
+print('%.3e, %.3e, %.3e, %.3e, %.3e' %(test_training.sess.run(test_velocity_model.loss_vel, feed_dict={test_velocity_model.vel_sample: test_training.sess.run(test_velocity_model.vel_NN)}), \
+    test_training.sess.run(test_velocity_model.loss_NS_x), test_training.sess.run(test_velocity_model.loss_NS_y), test_training.sess.run(test_velocity_model.loss_cont), k))
 
-p = ax.pcolor(X, Y, test_abs_vel_pred.reshape(x_plot.shape[0],-1), vmin=abs(test_abs_vel_pred).min(), vmax=abs(test_abs_vel_pred).max())
-cb = fig.colorbar(p)
-plt.savefig('unit_test.png')
+f, ax1 = plt.subplots(1, 1, figsize=(40,35))
+ax1.set_xlim(0.25,0.6)
+ax1.set_ylim(0,0.4)
+for i in range(initial.shape[0]):
+    ax1.arrow(initial[i,0], initial[i,1],(pred[i,0]-initial[i,0]), (pred[i,1]-initial[i,1]), width=0.0005, head_width=0.002)
+    ax1.scatter(train_data[:,[0,2]][i,:], train_data[:,[1,3]][i,:])
+plt.savefig('train_prediction.png')
+
+f, ax1 = plt.subplots(1, 1, figsize=(40,35))
+ax1.set_xlim(0.25,0.6)
+ax1.set_ylim(0,0.4)
+for i in range(test_initial.shape[0]):
+    ax1.arrow(test_initial[i,0], test_initial[i,1],(test_pred[i,0]-test_initial[i,0]), (test_pred[i,1]-test_initial[i,1]), width=0.0005, head_width=0.002)
+    ax1.scatter(test_data[:,[0,2]][i,:], test_data[:,[1,3]][i,:])
+plt.savefig('test_prediction.png')
+
+f, ax1 = plt.subplots(1, 1, figsize=(40,35))
+ax1.set_xlim(0.25,0.6)
+ax1.set_ylim(0,0.4)
+for i in range(test_initial.shape[0]):
+    ax1.arrow(test_initial[i,0], test_initial[i,1],(test_final[i,0]-test_initial[i,0]), (test_final[i,1]-test_initial[i,1]), width=0.0005, head_width=0.002)
+plt.savefig('test_true.png')
