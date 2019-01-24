dd315d9b331c23267b78078727985a2601e99c85
Sabareesh Mamidipaka <sabareesh169@gmail.com>
2019-01-23 23:49:01 -0500

    Update training_process.py

--- ptv_piv_dnn/training_process.py
+++ ptv_piv_dnn/training_process.py
@@ -29,11 +29,10 @@ class TrainingProcess:
         self.VelocityModel = VelocityModel
         self.optimizer_phy = tf.train.AdamOptimizer().minimize(self.VelocityModel.total_residue, var_list=self.VelocityModel.vel_weights+self.VelocityModel.vel_biases)
         self.optimizer_vel = tf.train.AdamOptimizer().minimize(self.VelocityModel.loss_vel, var_list=self.VelocityModel.vel_weights+self.VelocityModel.vel_biases)
-        self.optimizer_sigma = tf.train.AdamOptimizer().minimize(self.VelocityModel.neg_log_prob, var_list=self.VelocityModel.sigma)
-    
+        self.optimizer_sigma = tf.train.AdamOptimizer().minimize(self.VelocityModel.neg_log_prob, var_list=self.VelocityModel.dummy_var)
         init = tf.global_variables_initializer()
         self.sess.run(init)
-        
+
     def match_particles_and_velocity(self, n_iter):
         """
         This is to optimze in the case when the matching points are not known.
@@ -42,26 +41,29 @@ class TrainingProcess:
         :param size: Sampling the points, theta and sigma one after the other for n_iter number of times.
         :returns: optimized weights of the velocity DNN and the matched particles.
         """
-        
+
         for i in range(n_iter):
             index = sampling_points(self.ParticleData.initial_pos, self.ParticleData.final_pos, self.sess.run(self.VelocityModel.pos_NN), self.ParticleData.cluster, self.sess.run(self.VelocityModel.sigma))
             final_index = self.ParticleData.scale_pos_data(self.ParticleData.final_pos[index])
+            matched_points = 1-float(np.count_nonzero(np.arange(initial.shape[0])-index)) / initial.shape[0]
             sampling_theta(self, self.ParticleData.initial_pos_norm, final_index, self.ParticleData.t_initial_norm, self.ParticleData.t_final_norm)
-            sampling_sigma(self, self.sess.run(self.VelocityModel.pos_NN), final_index)
-        return index  
+            like, sigma =sampling_sigma(self, self.sess.run(self.VelocityModel.pos_NN), self.ParticleData.final_pos[index])
+#            print('iteration %i number of matched points:' %i, matched_points*100, 'likelihood:', like, 'sigma:', sigma)
+        return index, sigma
 
-    def train_velocity(self, n_iter):
+    def train_velocity(self, true_vel, n_iter):
         """
         This is to optimze in the case when the matching points are known.
-        We optmize theta, and sigma alternatively one after the other.
+        We optmize theta keeping sigma fixed at the initial value.
         
         :param size: Sampling the theta and sigma one after the other for n_iter number of times.
         :returns: optimized weights of the velocity DNN.
         """
-        
-        for i in range(n_iter):
-            sampling_theta(self, self.ParticleData.initial_pos_norm, self.ParticleData.final_pos_norm, self.ParticleData.t_initial_norm, self.ParticleData.t_final_norm)
-            sampling_sigma(self, self.sess.run(self.VelocityModel.pos_NN), self.ParticleData.final_pos)
+
+        true_vel_ch = true_vel * self.ParticleData.max_time/self.ParticleData.sigma_pos        
+        for i in range(n_iter*50):
+            self.sess.run(self.optimizer_vel, feed_dict={self.VelocityModel.vel_sample: true_vel_ch})
+#            sampling_theta(self, self.ParticleData.initial_pos_norm, self.ParticleData.final_pos_norm, self.ParticleData.t_initial_norm, self.ParticleData.t_final_norm)
 
     def match_points(self, true_vel, n_iter):
         """
@@ -75,9 +77,9 @@ class TrainingProcess:
         true_vel_ch = true_vel * self.ParticleData.max_time/self.ParticleData.sigma_pos
         for i in range(n_iter):
             optimize_theta(self, true_vel_ch)
-        return sampling_points(self.ParticleData.initial_pos, self.ParticleData.final_pos, self.sess.run(self.VelocityModel.pos_NN), self.ParticleData.cluster, self.ParticleData.radius)        
+        return sampling_points(self.ParticleData.initial_pos, self.ParticleData.final_pos, self.sess.run(self.VelocityModel.pos_NN), self.ParticleData.cluster, self.ParticleData.radius/15.), self.sess.run(self.VelocityModel.sigma)       
 
-    def vel_predict(self, t, x, y):
+    def vel_predict(self, t, pos):
         """
         This is to predict the velocity at given time and position.
         
@@ -85,12 +87,12 @@ class TrainingProcess:
         :returns: velocity at the particular position and time.
         """
         
-        return self.sess.run(self.VelocityModel.vel_predict(t,x,y))                   
+        return self.sess.run(self.VelocityModel.vel_predict(t,pos))                   
 
-    def pos_predict(self, t1, t2, x, y):
+    def pos_predict(self, t1, t2, pos):
         """
         This is to predict the particle position at final time given the initial position and time of particle.
         :param : the time and position of the point and the time at whoch the position is required.
         :returns: predicted position of the particle.
         """
-        return self.sess.run(self.VelocityModel.pos_predict(t1, t2, x, y))  
+        return self.sess.run(self.VelocityModel.pos_predict(t1, t2, pos))  
