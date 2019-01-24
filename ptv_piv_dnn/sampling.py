84c82f5aca7831075adfebaea2469782877d0cd4
Sabareesh Mamidipaka <sabareesh169@gmail.com>
2019-01-23 23:48:23 -0500

    Update sampling.py

--- ptv_piv_dnn/sampling.py
+++ ptv_piv_dnn/sampling.py
@@ -9,50 +9,28 @@ Date:
 
 
 def sampling_points(initial, final, pred, list_, sigma):
-    """
-    Sampling the points by assigning a pmf according to the current prediction of the DNN.
-
-    :param array: the initial and final position, and the prediction and variance of the DNN
-    :returns: matching index for each point in the initial data.
-    """
-        
     index = np.arange(initial.shape[0])
     for i in range(initial.shape[0]):
         points_index = np.array(list_[i])
         points = final[points_index]
-        p = np.exp(-np.sum((pred[i,:]-points)**2, axis=1)/(2*sigma**2))/(2*sigma)
-        cum_sum_q=np.exp(np.log(p)-sc.misc.logsumexp(np.log(p))).cumsum()
+        log_p = -np.sum((pred[i,:]-points)**2, axis=1)/(2*sigma**2) 
+        cum_sum_q=np.exp(log_p-sc.misc.logsumexp(log_p)).cumsum()
         u=np.random.rand()
         idx=np.arange(len(cum_sum_q))
         index[i]=points_index[idx[u<cum_sum_q][0]]
     return index
 
-def optimize_theta(self, true_vel_ch):
-    """
-    Optimizing the DNN when the sample velocity is given.
-
-    :param array: the current 'true velocity' given the matching index.
-    """
-    
-    for i in range(50):
+def optimize_theta(self, true_vel_ch, n_iter):
+    for i in range(n_iter):
         self.sess.run(self.optimizer_phy)
         self.sess.run(self.optimizer_vel, feed_dict={self.VelocityModel.vel_sample: true_vel_ch})
+        self.sess.run(self.optimizer_vel, feed_dict={self.VelocityModel.vel_sample: true_vel_ch})
 
-def sampling_theta(self, initial, final_index, t_initial, t_final):
-    """
-    Optimizing the DNN when the sample velocity is given.
-
-    :param array: the initial and final position, and the prediction and variance of the DNN
-    :returns: matching index for each point in the initial data.
-    """
+def sampling_theta(self, initial, final_index, t_initial, t_final, n_iter=50):
     true_vel_ch = (final_index-initial)/(t_final-t_initial)
-    optimize_theta(self, true_vel_ch)
+    optimize_theta(self, true_vel_ch, n_iter)
 
 def sampling_sigma(self, pred, final_index):
-    """
-    Optimizing the variance given the likelihood.
-
-    :param array: the optimzer for variance, prediction and matched index given by the DNN
-    """
-    for i in range(50):
+    for i in range(100):
         self.sess.run(self.optimizer_sigma, feed_dict={self.VelocityModel.likelihood : np.sum((pred-final_index)**2)})
+    return np.sum((pred-final_index)**2), self.sess.run(self.VelocityModel.sigma)
