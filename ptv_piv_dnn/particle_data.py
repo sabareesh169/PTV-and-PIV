f317df401d7bd5eaaccd007f889e9f6cfeef8278
Sabareesh Mamidipaka <sabareesh169@gmail.com>
2019-02-24 17:29:16 -0500

    Update particle_data.py

--- ptv_piv_dnn/particle_data.py
+++ ptv_piv_dnn/particle_data.py
@@ -23,38 +23,39 @@ class ParticleData(object):
     functioning of the DNN's.
     The postional data is normalised w.r.t the initial position of the particles. 
     The time data is 'normalized' differently to avoid (potentially) division with zero.
-    The position data is then used to form multiple clusters to limit the possible 
-    matches for each initial point.
     
     """
     
-    def __init__(self, initial_pos, final_pos, t_initial, t_final, radius):
+    def __init__(self, initial_pos, final_pos, t_initial, t_final):
         """
         
-        :param : The initial and final positions of particles, the initial and final 
-        time instants, and the radius of cluster
-        """        
-        self.sess = tf.Session()
+        :param : The initial and final positions of particles as an array, the initial and final 
+        time instants as an array, and the radius of cluster
+        
+        """
+        
+        ## Storing the given data
         self.initial_pos = initial_pos.astype(np.float32)
         self.final_pos = final_pos.astype(np.float32)
         self.t_initial = np.float32(t_initial)
         self.t_final = np.float32(t_final)
         
+        ## Calculate and store the statistics of the data
         self.mean_pos = np.mean(initial_pos,axis=0)
-        self.sigma_pos = np.std(initial_pos,axis=0)
-        self.radius = radius
 
+        self.min_pos = np.min(np.concatenate([initial_pos, final_pos],axis=0),axis=0)
+        self.max_pos = np.max(np.concatenate([initial_pos, final_pos],axis=0),axis=0)
+        self.range = np.max_pos-self.min_pos
+        
         self.max_time = np.max(self.t_final)
         self.min_time = np.min(self.t_initial)
         self.t_initial_norm = self.scale_time_data(t_initial).astype(np.float32)
         self.t_final_norm = self.scale_time_data(t_final).astype(np.float32)
+        self.time_bound = [np.min(self.t_initial_norm), np.max(self.t_final_norm)]
 
+        ## Normalizing the data
         self.initial_pos_norm = self.scale_pos_data(initial_pos).astype(np.float32)
         self.final_pos_norm = self.scale_pos_data(final_pos).astype(np.float32)
-
-        tree = spatial.KDTree(initial_pos)
-        self.cluster = tree.query_ball_point(initial_pos, radius)
-        self.time_bound = [np.min(self.t_initial_norm), np.max(self.t_final_norm)]
         
     def scale_pos_data(self, array):
         """
@@ -63,7 +64,7 @@ class ParticleData(object):
         :param array: any spatial data to be normalized w.r.t the mean and variance of the initial position
         :returns: normalized data
         """
-        normalized_data = ((array - self.mean_pos) / self.sigma_pos)
+        normalized_data = ((array - self.min_pos) / self.range)
         return normalized_data
     
     def scale_time_data(self, time):
